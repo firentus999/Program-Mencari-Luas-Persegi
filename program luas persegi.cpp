20701ef734194cfa717700f3071ebf6ea743c5a3
Firentus Agustone Pasaribu <45529731+firentus999@users.noreply.github.com>
2018-12-14 12:57:17 +0700

    Program

---
+++ program luas persegi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"======PROGRAM MENCARI LUAS PERSEGI====="<<endl;
+    int p,l,luas;
+    cout<<"Masukan panjang :";
+    cin>>p;
+    cout<<"Masukan lebar :";
+    cin>>l;
+    luas=p*l;
+    cout<<"luas persegi adalah "<<luas;
+    return 0;
+}
